04b05382fca939f58539243576c25aaf5f73c6e5
XiaoWang <1489201050@qq.com>
2023-01-12 00:40:14 +0800

    Add files via upload

---
+++ hm_05_函数的参数.py
@@ -0,0 +1,10 @@
+def sum_2_num(num1, num2):
+    """对两个数字的求和"""
+
+
+    result = num1 + num2
+
+    print("%d + %d = %d" %(num1, num2, result))
+
+
+sum_2_num(-5, 20)
---
+++ hm_07_函数的嵌套调用.py
@@ -0,0 +1,15 @@
+def test1():
+
+    print("*" * 50)
+
+
+def test2():
+
+    print("-" * 50)
+
+    test1()
+
+    print("+" * 50)
+
+
+test2()
